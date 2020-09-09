ee4a798d731683ac7f6db7cb36cfaa427f7a868c
pvahanian <51976194+pvahanian@users.noreply.github.com>
2020-09-08 17:14:42 -0700

    Mostly Done, crashes on like click

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -6,11 +6,10 @@ const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
 
-  return (
-    <div>
-      {/* map through the comments prop and render a Comment for every piece of data */}
-    </div>
+  return (<div>{comments.map(comment =><Comment key={comment.id} comment={comment}/>)}</div>
   );
 };
 
 export default Comments;
+{/* map through the comments prop and render a Comment for every piece of data */}
+
--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -5,7 +5,7 @@ import PostHeader from './PostHeader';
 
 const Post = props => {
   // 🔥 Make sure the parent of Post is passing the right props!
-  const { post, likePost } = props;
+  const { post, likePost} = props;
 
   return (
     <div className='post-border'>
@@ -21,11 +21,12 @@ const Post = props => {
         />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
+      <LikeSection  likePost={likePost} postId={post.id} numberOfLikes={post.likes} />
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments comments={post.comments}/>
     </div>
   );
 };
 
 export default Post;
+
